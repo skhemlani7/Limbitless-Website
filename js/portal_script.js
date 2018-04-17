1f174b4ab4bd50d970689f3e47bed1f120a70d0f
Sarvesh Khemlani <sarveshkhemlani@gmail.com>
2018-04-17 18:52:42 -0400

    Added scrolling functionality to nav bar

--- js/portal_script.js
+++ js/portal_script.js
@@ -8,9 +8,47 @@
                 firebase.auth().signOut();
                 e.preventDefault();
             });
+            document.getElementById('home').addEventListener('click', function(e){
+                smoothScroll(document.getElementById('sectionOne'));
+                e.preventDefault();
+            });
+            document.getElementById('games').addEventListener('click', function(e){
+                smoothScroll(document.getElementById('sectionTwo'));
+                e.preventDefault();
+            });
+            document.getElementById('devices').addEventListener('click', function(e){
+                smoothScroll(document.getElementById('sectionDevices'));
+                e.preventDefault();
+            });
+
         } else {
           window.location = 'index.html';
         }
     });
 
 }());
+
+//Scroll function for nav bar
+//onclick="smoothScroll(document.getElementById('second'))"
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
